b7933bc2da4af78e9785cff12706bf2a94a39bc5
xstefank <xstefank122@gmail.com>
2017-10-08 14:44:42 +0200

    add query-service project

---
+++ query-service/src/main/java/org/learn/axonframework/queryservice/QueryServiceApplication.java
@@ -0,0 +1,35 @@
+package org.learn.axonframework.queryservice;
+
+import com.rabbitmq.client.Channel;
+import org.axonframework.amqp.eventhandling.DefaultAMQPMessageConverter;
+import org.axonframework.amqp.eventhandling.spring.SpringAMQPMessageSource;
+import org.axonframework.serialization.Serializer;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class QueryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueryServiceApplication.class, args);
+	}
+
+	@Bean
+	public SpringAMQPMessageSource queryEvents(Serializer serializer) {
+		return new SpringAMQPMessageSource(new DefaultAMQPMessageConverter(serializer)) {
+
+			@RabbitListener(queues = "OrderEvents")
+			@Override
+			public void onMessage(Message message, Channel channel) throws Exception {
+				//necessary because message is not delivered to saga otherwise
+				LoggerFactory.getLogger(QueryServiceApplication.class).info("message - " + message.toString());
+				Thread.sleep(1000);
+				super.onMessage(message, channel);
+			}
+		};
+	}
+}
