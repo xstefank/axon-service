7f7a83665669558800b239a399a04f743d2e58e2
xstefank <xstefank122@gmail.com>
2018-01-20 15:53:37 +0100

    update Query API for individual objects

--- query-service/src/main/java/org/learn/axonframework/queryservice/rest/QueryController.java
+++ query-service/src/main/java/org/learn/axonframework/queryservice/rest/QueryController.java
@@ -7,12 +7,15 @@ import org.learn.axonframework.queryservice.repository.InvoiceRepository;
 import org.learn.axonframework.queryservice.repository.OrderRepository;
 import org.learn.axonframework.queryservice.repository.ShipmentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
+@RestController("/api")
 public class QueryController {
 
     @Autowired
@@ -24,9 +27,26 @@ public class QueryController {
     @Autowired
     private InvoiceRepository invoiceRepository;
 
+    @GetMapping("/orders")
+    public List<Order> getOrders() {
+        return orderRepository.findAll();
+    }
+
+    @GetMapping("/order/{orderId}")
+    public ResponseEntity<Order> getOrder(@PathVariable String orderId) {
+        Order order = orderRepository.findOne(orderId);
+        return order != null ? ResponseEntity.ok(order) : ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    }
+
     @GetMapping("/shipments")
     public List<Shipment> getShipments() {
         return shipmentRepository.findAll();
+    }
+
+    @GetMapping("/shipment/{shipmentId}")
+    public ResponseEntity<Shipment> getShipment(@PathVariable String shipmentId) {
+        Shipment shipment = shipmentRepository.findOne(shipmentId);
+        return shipment != null ? ResponseEntity.ok(shipment) : ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
     }
 
     @GetMapping("/invoices")
@@ -34,8 +54,9 @@ public class QueryController {
         return invoiceRepository.findAll();
     }
 
-    @GetMapping("/orders")
-    public List<Order> getOrders() {
-        return orderRepository.findAll();
+    @GetMapping("/invoice/{invoiceId}")
+    public ResponseEntity<Invoice> getInvoice(String invoiceId) {
+        Invoice invoice = invoiceRepository.findOne(invoiceId);
+        return invoice != null ? ResponseEntity.ok(invoice) : ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
     }
 }
